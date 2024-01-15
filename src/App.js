76bebf7e02f98de92df3e9a2629827a44d28ec47
SyS <sreyassreelal@gmail.com>
2024-01-15 16:54:46 +0530

    some ui alignment fixes

--- src/App.js
+++ src/App.js
@@ -47,67 +47,81 @@ export default function App() {
   }
 
   return (
-    <Box>
-      <Box textAlign={"center"}>
-        <Typography variant='h3'>
-          Malluscript Online Interpreter
-        </Typography>
-        <Typography variant='subtitle'>{version}</Typography>
-        {versionError !== "" ?
-          <Alert variant="filled" severity="error">
-            {versionError}
-          </Alert> : ''}
-      </Box>
-      <Box m={0.3} border={1} borderColor={"black"}>
-        <Grid container>
-          <Grid xs={12} item>
-            <Box sx={{ backgroundColor: "#161b22" }}>
-              <Box color="#061010">
-                <Button variant='outlined' onClick={sendRequest}><PlayArrowIcon /></Button>
-              </Box>
-              <div style={{ height: 300, overflow: "auto" }}>
-                <CodeEditor
-                  value={code}
-                  language="c"
-                  placeholder="Please enter Malluscript code."
-                  onChange={storeCode}
-                  padding={15}
-                  style={{
-                    fontSize: 16,
-                    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-                  }}
-                  maxHeight={300}
-                  minHeight={300}
-                  minLength={300}
-                />
-              </div>
-            </Box>
-          </Grid>
+    <>
+      <Grid container>
+        {/* Title */}
+        <Grid xs={12} item>
+          <Box textAlign={"center"}>
+            <Typography variant='h3'>
+              Malluscript Online Interpreter
+            </Typography>
+            <Typography variant='subtitle'>{version}</Typography>
+            {versionError !== "" ?
+              <Alert variant="filled" severity="error">
+                {versionError}
+              </Alert> : ''}
+          </Box>
         </Grid>
-      </Box>
-      <Box ml={1} mr={1}>
-        <Grid container spacing={2}>
-          <Grid md={4} xs={12} item>
-            <Typography variant='h6'> Input </Typography>
-            <Box border={1} borderColor={"black"}>
-              <TextField inputProps={{ style: { color: "white" } }} sx={{ height: "100%", width: "100%" }} multiline variant='filled' rows={10} onInput={(e) => setInput(e.target.value)} />
-            </Box>
-          </Grid>
-          <Grid md={8} xs={12} item>
+        {/* Input Area */}
+        <Grid xs={12} item>
+          <Box m={0.3} border={1} borderColor={"black"}>
+            <Grid container>
+              <Grid xs={12} item>
+                <Box sx={{ backgroundColor: "#161b22" }}>
+                  <Box color="#061010">
+                    <Button variant='outlined' onClick={sendRequest}><PlayArrowIcon /></Button>
+                  </Box>
+                  <div style={{ height: 300, overflow: "auto" }}>
+                    <CodeEditor
+                      value={code}
+                      language="c"
+                      placeholder="Please enter Malluscript code."
+                      onChange={storeCode}
+                      padding={15}
+                      style={{
+                        fontSize: 16,
+                        fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                      }}
+                      maxHeight={300}
+                      minHeight={300}
+                      minLength={300}
+                    />
+                  </div>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        {/* Output Area */}
+        <Grid xs={12} item>
+          <Box ml={1} mr={1}>
+            {/* <Grid container spacing={2}>
+            <Grid md={4} xs={12} item>
+              <Typography variant='h6'> Input </Typography>
+              <Box border={1} borderColor={"black"}>
+                <TextField inputProps={{ style: { color: "white" } }} sx={{ height: "100%", width: "100%" }} multiline variant='filled' rows={10} onInput={(e) => setInput(e.target.value)} />
+              </Box>
+            </Grid> */}
+            {/* <Grid md={8} xs={12} item> */}
             <Typography variant='h6'> Output </Typography>
-            <Box height={"89.3%"} sx={{ backgroundColor: "black", whiteSpace: "pre-line" }} color={"green"} border={'black'}>
+            <Box height={"30vh"} sx={{ backgroundColor: "black", whiteSpace: "pre-line" }} color={"green"} border={'black'}>
               {error === "" ? output : <Alert variant="outlined" severity="error">
                 {error}
               </Alert>}
             </Box>
-          </Grid>
+            {/* </Grid>
+          </Grid> */}
+          </Box>
         </Grid>
-      </Box>
-      <Box mt={0.7} textAlign={'center'}>
-        <Link href='https://github.com/sreyas-sreelal/malluscript'>
-          <Typography variant='subtitle'>Github Repository</Typography>
-        </Link>
-      </Box>
-    </Box>
+        <Grid item xs={12}>
+          <Box mt={0.7} textAlign={'center'} bottom={0} position={'sticky'}>
+            <Link href='https://github.com/sreyas-sreelal/malluscript'>
+              <Typography variant='subtitle'>Github Repository</Typography>
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+
+    </>
   );
 }
