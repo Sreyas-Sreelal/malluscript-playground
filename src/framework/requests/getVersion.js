6eef28dd4d6c8ac27287370b7c8e55a74cd439ca
SyS <sreyassreelal@gmail.com>
2024-01-15 15:53:22 +0530

    cleanup a bit

--- src/framework/requests/getVersion.js
+++ src/framework/requests/getVersion.js
@@ -1,5 +1,3 @@
-import sendRequest from "../sendRequest";
-
 export default async function getVersion()  {
     return '1.1.0';
 }
--- src/framework/requests/sendRunRequest.js
+++ src/framework/requests/sendRunRequest.js
@@ -1,8 +1,7 @@
-import sendRequest from "../sendRequest";
 import init, { run_malluscript } from "../malluscript/malluscript_wasm.js";
 
 export default async function sendRunRequest(code, input) {
   await init();
-  let data = await run_malluscript(code);
+  let data = run_malluscript(code);
   return data;
 }
